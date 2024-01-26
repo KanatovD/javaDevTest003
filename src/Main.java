34e5bf31ce8a86cd7aca2de82aa96b5bda0893a7
temaprof <kanatovd55@mail.com>
2024-01-26 23:01:39 +0600

    test 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("test git");
+        System.out.println("test 1");
+        System.out.println("test 2");
 
     }
 }
