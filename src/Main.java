6ccb4c18838e5a73032d13f1410e4696fcb747fe
temaprof <kanatovd55@mail.com>
2024-01-26 23:17:50 +0600

    adds 2 more lines

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
         System.out.println("test git");
         System.out.println("test 1");
         System.out.println("test 2");
+        System.out.println("test 3");
+        System.out.println("test 4");
 
     }
 }
